cc565576c8c72a2df212bd1e29cbeb6e1314e87e
Matej Kováč <kovo.mato@gmail.com>
2021-09-17 19:11:17 +0200

    initial commit

---
+++ src/test/java/com/kovospace/paster/PasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovospace.paster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
