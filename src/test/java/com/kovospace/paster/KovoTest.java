dbd2c14452cf2f8e76bfe030bd2814c905daada2
Matej Kováč <kovo.mato@gmail.com>
2021-12-09 00:56:53 +0100

    opravene integracne testy

--- src/test/java/com/kovospace/paster/KovoTest.java
+++ src/test/java/com/kovospace/paster/KovoTest.java
@@ -12,20 +12,24 @@ import java.lang.reflect.Method;
 import java.util.function.Function;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase
 @TestMethodOrder(OrderAnnotation.class)
+// because websockets
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class KovoTest {
 
   protected String API_PREFIX;
