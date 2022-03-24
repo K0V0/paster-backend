3ea0f118fa57f0c4ea54d46a80f6b2c7e4da1e47
Matej Kováč <kovo.mato@gmail.com>
2022-03-24 23:24:51 +0100

    adding cross origin settings

--- src/main/java/com/kovospace/paster/user/controllers/UserController.java
+++ src/main/java/com/kovospace/paster/user/controllers/UserController.java
@@ -9,11 +9,13 @@ import com.kovospace.paster.user.exceptions.UserException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController extends BaseController {
