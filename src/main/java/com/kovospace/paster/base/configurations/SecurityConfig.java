3e9ad287fb197b4f18cd270f731831e85797e1c5
Matej Kováč <kovo.mato@gmail.com>
2022-05-02 22:01:25 +0200

    adding subdomains to crossOrigins

--- src/main/java/com/kovospace/paster/base/configurations/SecurityConfig.java
+++ src/main/java/com/kovospace/paster/base/configurations/SecurityConfig.java
@@ -58,12 +58,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // TODO ulozit allowed origins niekde do configu
     configuration.setAllowedOrigins(Arrays.asList(
             "http://0.0.0.0:4200",
+            "http://0.0.0.0:6060",
+            "http://kovo.space:4200",
             "http://kovo.space:6060",
             "http://localhost:4200",
             "http://localhost:6060",
-            "http://0.0.0.0:6060",
             "http://paster.cloud",
-            "https://paster.cloud"
+            "http://www.paster.cloud",
+            "https://paster.cloud",
+            "https://www.paster.cloud"
     ));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
