aaf19a813790c40bfce337476d9c0928b85e45c3
Matej Kováč <kovo.mato@gmail.com>
2022-05-29 01:48:13 +0200

    FIX websocket handshake exception in security

--- src/main/java/com/kovospace/paster/base/configurations/securityConfig/ApiKeySecurityConfig.java
+++ src/main/java/com/kovospace/paster/base/configurations/securityConfig/ApiKeySecurityConfig.java
@@ -7,6 +7,7 @@ import com.kovospace.paster.base.services.ApiKeyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -43,11 +44,16 @@ public class ApiKeySecurityConfig extends BaseSecurityConfig {
         });
 
         httpSecurity
-                .antMatcher("/**")
                 .csrf().disable()
                 .cors()
                 .and()
+                .exceptionHandling()
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/websocket").permitAll()
+                .antMatchers("/stomp").permitAll()
                 .and()
                 .addFilterBefore(
                         exceptionsFilter,
