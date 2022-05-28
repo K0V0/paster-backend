a789ac6d6040ce47cfe0bd1331e0e725ed89bf1a
Matej Kováč <kovo.mato@gmail.com>
2022-05-28 03:10:59 +0200

    FIX CORS settings due intercepting filters order

--- src/main/java/com/kovospace/paster/base/configurations/securityConfig/ApiKeySecurityConfig.java
+++ src/main/java/com/kovospace/paster/base/configurations/securityConfig/ApiKeySecurityConfig.java
@@ -11,13 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
 @Order(1)
-public class ApiKeySecurityConfig extends WebSecurityConfigurerAdapter {
+public class ApiKeySecurityConfig extends BaseSecurityConfig {
 
     private BaseService baseService;
     private ApiKeyService apiKeyService;
