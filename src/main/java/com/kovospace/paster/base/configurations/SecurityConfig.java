f9e6cdb8b95a993b01c1ddb9e0c5a26764954524
Matej Kováč <kovo.mato@gmail.com>
2022-05-30 00:58:32 +0200

    FIX refactor securityConfig & filters 2

--- src/main/java/com/kovospace/paster/base/configurations/SecurityConfig.java
+++ src/main/java/com/kovospace/paster/base/configurations/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.kovospace.paster.base.exceptions.FiltersExceptionHandler;
 import com.kovospace.paster.base.filters.ApiKeyAuthFilter;
 import com.kovospace.paster.base.filters.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
@@ -12,7 +13,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
@@ -62,7 +67,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(
                         jwtAuthFilter,
                         UsernamePasswordAuthenticationFilter.class);
+    }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // TODO ulozit allowed origins niekde do configu
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://0.0.0.0:4200",
+                "http://0.0.0.0:6060",
+                "http://kovo.space:4200",
+                "http://kovo.space:6060",
+                "http://localhost:4200",
+                "http://localhost:6060",
+                "http://paster.cloud",
+                "http://www.paster.cloud",
+                "https://paster.cloud",
+                "https://www.paster.cloud"
+        ));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        //configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
