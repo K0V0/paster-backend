36d5da5000e75508d89887e1ae8431ed75467b38
Matej Kováč <kovo.mato@gmail.com>
2022-06-05 16:44:43 +0200

    CHANGE errors response format for fields validation & data errors type

--- src/main/java/com/kovospace/paster/base/dtos/ErrorsResponseDTO.java
+++ src/main/java/com/kovospace/paster/base/dtos/ErrorsResponseDTO.java
@@ -3,15 +3,16 @@ package com.kovospace.paster.base.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.Map;
 
 @Getter
 @Setter
 public class ErrorsResponseDTO extends ResponseDTO {
 
-  private Map<String, Map<String, String>> messages;
+  private Map<String, List<ErrorResponseDTO>> messages;
 
-  public ErrorsResponseDTO(Map<String, Map<String, String>> messages) {
+  public ErrorsResponseDTO(Map<String, List<ErrorResponseDTO>> messages) {
     super("error");
     this.messages = messages;
   }
--- src/main/java/com/kovospace/paster/user/exceptions/UserLoginBadCredentialsException.java
+++ src/main/java/com/kovospace/paster/user/exceptions/UserLoginBadCredentialsException.java
@@ -2,5 +2,5 @@ package com.kovospace.paster.user.exceptions;
 
 public class UserLoginBadCredentialsException extends UserException {
 
-
+    public UserLoginBadCredentialsException() { super("user.login.credentials.wrong"); }
 }
